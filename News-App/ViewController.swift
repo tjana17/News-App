7b135cd41ca09791af14a60e3847a3390bc3d27c
janarthanan <janarthanan@yujdesigns.com>
2024-05-25 10:50:58 +0530

    Initial Commit

---
+++ News-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News-App
+//
+//  Created by DB-MBP-023 on 25/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
